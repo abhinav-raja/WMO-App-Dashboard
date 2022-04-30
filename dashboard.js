91495e415a9dfce09a7b3661f1405f225e8092f0
Random Name <abhinav.raja03@gmail.com>
2022-05-01 01:24:09 +0530

    To do panel added and Budget updated

--- dashboard.js
+++ dashboard.js
@@ -1,20 +1,7 @@
-const budgetCanvas = document.getElementById('budgetChart');
-const budgetChart = new Chart(budgetCanvas, {
-	type: 'pie',
-	data: {
-		labels: ['Spent', 'Remaining'],
-		datasets: [{
-			label: 'This month',
-			data: [2000, 1000],
-			backgroundColor: ['rgb(235,49,86)', 'rgb(47,191,113)']
-		}]
-	},
-	options: {
-		//cutout: "70%"
-		plugins: {
-			legend: {
-				display: false,
-			}
-		}
-	}
-})
+const budgetLine = new ProgressBar.Line('#budgetLine', {
+	strokeWidth: 10,
+	trailColor: '#2fbf71',
+	color: '#3d348b',
+});
+
+budgetLine.animate(0.67, {easing: 'easeInOut'});
