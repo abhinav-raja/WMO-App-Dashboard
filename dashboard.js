0f885f96b2a849a132960a9821a7f076c0f19e86
Random Name <abhinav.raja03@gmail.com>
2022-05-02 00:15:04 +0530

    Notifications and Habit Panels Layout Done

--- dashboard.js
+++ dashboard.js
@@ -5,3 +5,35 @@ const budgetLine = new ProgressBar.Line('#budgetLine', {
 });
 
 budgetLine.animate(0.67, {easing: 'easeInOut'});
+
+const codingLine = new ProgressBar.Line('#codingLine', {
+	strokeWidth: 7,
+	color: '#067bc2',
+	trailColor: '#d9d9d9',
+});
+
+codingLine.animate(0.67, {easing: 'easeInOut'});
+
+const waterLine = new ProgressBar.Line('#waterLine', {
+	strokeWidth: 7,
+	color: '#067bc2',
+	trailColor: '#d9d9d9',
+});
+
+waterLine.animate(0.5, {easing: 'easeInOut'});
+
+const studyLine = new ProgressBar.Line('#studyLine', {
+	strokeWidth: 7,
+	color: '#067bc2',
+	trailColor: '#d9d9d9',
+});
+
+studyLine.animate(0.86, {easing: 'easeInOut'});
+
+const walkLine = new ProgressBar.Line('#walkLine', {
+	strokeWidth: 7,
+	color: '#067bc2',
+	trailColor: '#d9d9d9',
+});
+
+walkLine.animate(0.33, {easing: 'easeInOut'});
