418894734521f5da0628c63788a58fdfa1481053
Random Name <abhinav.raja03@gmail.com>
2022-05-02 17:09:00 +0530

    Styling updated

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,14 @@
 const budgetLine = new ProgressBar.Line('#budgetLine', {
 	strokeWidth: 10,
 	trailColor: '#2fbf71',
-	color: '#3d348b',
+	color: '#ef2d56',
 });
 
 budgetLine.animate(0.67, {easing: 'easeInOut'});
 
 const codingLine = new ProgressBar.Line('#codingLine', {
 	strokeWidth: 7,
-	color: '#067bc2',
+	color: '#ff7d00',
 	trailColor: '#d9d9d9',
 });
 
@@ -16,7 +16,7 @@ codingLine.animate(0.67, {easing: 'easeInOut'});
 
 const waterLine = new ProgressBar.Line('#waterLine', {
 	strokeWidth: 7,
-	color: '#067bc2',
+	color: '#39a0ed',
 	trailColor: '#d9d9d9',
 });
 
@@ -24,7 +24,7 @@ waterLine.animate(0.5, {easing: 'easeInOut'});
 
 const studyLine = new ProgressBar.Line('#studyLine', {
 	strokeWidth: 7,
-	color: '#067bc2',
+	color: '#ff7d00',
 	trailColor: '#d9d9d9',
 });
 
@@ -32,7 +32,7 @@ studyLine.animate(0.86, {easing: 'easeInOut'});
 
 const walkLine = new ProgressBar.Line('#walkLine', {
 	strokeWidth: 7,
-	color: '#067bc2',
+	color: '#39a0ed',
 	trailColor: '#d9d9d9',
 });
 
